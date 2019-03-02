9df029d647ea10cafd40befd5a1c380d3942c0c9
bobanko <mishko.vladimir@gmail.com>
2019-03-02 22:32:15 +0200

    monster follow targets impl

--- towerDefence/components/monster.js
+++ towerDefence/components/monster.js
@@ -1,5 +1,6 @@
 import GameComponent from "./game-component.js";
 import { delay } from "./index.js";
+import { collidePointWithCircle } from "../collision.js";
 
 export default class Monster extends GameComponent {
   constructor(props) {
@@ -9,39 +10,36 @@ export default class Monster extends GameComponent {
   }
 
   async ai() {
-    while (true) {
-      if (this.y > 600 - this.size) {
-        this.vy *= -1;
-      }
-      if (this.x > 800 - this.size) {
-        this.vx *= -1;
-      }
-      if (this.y < 0) {
-        this.vy *= -1;
-      }
-      if (this.x < 0) {
-        this.vx *= -1;
+    let a = { x: 0, y: 0 };
+    let b = { x: 0, y: 600 };
+    let c = { x: 600, y: 0 };
+    let d = { x: 200, y: 100 };
+
+    this.targets = [a, b, c, d];
+
+    for (let target of this.targets) {
+      while (
+        !collidePointWithCircle(target, {
+          cx: this.x,
+          cy: this.y,
+          r: this.size
+        })
+      ) {
+        let distance = { x: target.x - this.x, y: target.y - this.y };
+
+        let length = Math.sqrt(distance.x ** 2 + distance.y ** 2);
+
+        let direction = { x: distance.x / length, y: distance.y / length };
+
+        //for (;;) {}
+
+        this.x += direction.x * this.speed;
+        this.y += direction.y * this.speed;
+
+        await delay(10);
       }
 
-      this.x += this.vx;
-      this.y += this.vy;
-      //wait 1s
-      await delay(10);
+      await delay(1000);
     }
-  }
-
-  update() {
-    // if (this.y > 600 - this.size) {
-    //   this.vy *= -1;
-    // }
-    // if (this.x > 800 - this.size) {
-    //   this.vx *= -1;
-    // }
-    // if (this.y < 0) {
-    //   this.vy *= -1;
-    // }
-    // if (this.x < 0) {
-    //   this.vx *= -1;
-    // }
   }
 }
